99cdebfc8f90ed71ae7c25ab7eba1798b097621a
shielaQAQ <21121513@bjtu.edu.cn>
2023-08-07 08:45:24 -0400

    new

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,7 +4,7 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
@@ -25,7 +25,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
