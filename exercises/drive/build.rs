6c69420a9abf6bc759844d665d6e0743c42cc5e9
shielaQAQ <21121513@bjtu.edu.cn>
2023-08-13 03:35:23 -0400

    new12

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,10 +1,8 @@
 fn main() {
     #[cfg(feature = "pass")]
-    {
-        // Get the current timestamp
-        let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+    // Get the current timestamp
+    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
 
-        // Set the environment variable TEST_FOO to the current timestamp
-        println!("cargo:rustc-env=TEST_FOO={}", timestamp);
-    }
+    // Set the environment variable TEST_FOO to the current timestamp
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 }
