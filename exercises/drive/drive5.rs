eb96c8abf2d232a18fe1173d641121c5b48b4afe
shielaQAQ <21121513@bjtu.edu.cn>
2023-08-13 03:58:47 -0400

    new13

--- exercises/drive/drive5.rs
+++ exercises/drive/drive5.rs
@@ -5,19 +5,29 @@
 // You should not modify any existing code. All you need to do is add two line of attributes.
 
 
+
+
+
 extern {
     fn my_demo_function(a:u32) -> u32;
     #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a:u32) -> u32;
 }
 
+
+
+
 mod Foo{
-    pub fn my_demo_function(a:u32) -> u32 {a}
+    fn my_demo_function(a:u32) -> u32 {a}
 }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[link(name = "Foo")]
     #[test]
     fn test_success() {
         unsafe {
